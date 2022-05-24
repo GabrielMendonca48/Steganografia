d1ec482d720ac788cddb6e2a3bed5e94a543cf6e
GabrielMendonca48 <100357167+GabrielMendonca48@users.noreply.github.com>
2022-05-24 11:01:33 +0200

    Update MainCmd.java

--- src/MainCmd.java
+++ src/MainCmd.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class MainCmd {
 
-    private void printMenu(){
-
+    public static void printMenu(){
+        System.out.println("**************************");
+        System.out.println("* 0) Esci");
+        System.out.println("* 1) Cifra");
+        System.out.println("* 2) Decifra");
+        System.out.println("* 3) Boh");
+        System.out.print("Scegli opzione : ");
     }
-
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do{
+            printMenu();
+            option = scanner.nextInt();
+        }while (option!=0);
+
+        scanner.close();
     }
 }
