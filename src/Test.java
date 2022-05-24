90be95fdea014be6720d84144599b78f7e7f85e4
Alexandro Di Nicola <alexandro.dinicola@student.supsi.ch>
2022-05-24 11:04:58 +0200

    inizio test mp3

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Test {
+
+    public static void main(String[] args) {
+
+        File mp3 = new File("bensound-anewbeginning.mp3");
+
+        try {
+            FileOutputStream fo = new FileOutputStream(mp3);
+
+            System.out.println(fo);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
